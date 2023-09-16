195e786a175b35c999a9774b41f7d1d97812eae1
subbarayudu <subbbarayudubadvel@gmail.com>
2023-09-17 01:01:05 +0530

    added additional checks..,

--- src/Test2.java
+++ src/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		//added test case 2 by badveli
 		String hungry ="yes";
-		if(hungry=="yes") {
+		if(hungry=="yes" || hungry =="Y" || hungry=="y") {
 			System.out.println("eat something");
 		}
 		else {
