ec924b8bae923ca6985883a0b29800f0c808c8d0
subbarayudu <subbbarayudubadvel@gmail.com>
2023-09-16 22:15:57 +0530

    My first commit created test class

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		String hungry ="yes";
+		if(hungry=="yes") {
+			System.out.println("eat something");
+		}
+		else {
+			System.out.println("Do work....");
+		}
+	}
+
+}
