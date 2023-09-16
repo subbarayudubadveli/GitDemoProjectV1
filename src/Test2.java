87f90470dd5e1fe19e1fe1ec06f3c8694556dafa
subbarayudu <subbbarayudubadvel@gmail.com>
2023-09-17 00:08:12 +0530

    Committing Test2 to branch future

---
+++ src/Test2.java
@@ -0,0 +1,15 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		//added test case 2 by badveli
+		String hungry ="yes";
+		if(hungry=="yes") {
+			System.out.println("eat something");
+		}
+		else {
+			System.out.println("Do work....");
+		}
+	}
+
+}
